ff80534d226db02c74abc9d2ae30942c83e88232
Jonathan Fowlke <FowlkeJL@churchofjesuschrist.org>
2022-10-02 15:14:05 -0600

    add storybook

---
+++ components/Banner/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Banner = ({ ...props }) => {
+    return <div {...props}>{"A Jack Reacher Map"}</div>;
+};
+
+export default Banner;
---
+++ components/Banner/stories.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Banner from "./.";
+
+export default {
+    title: "Banner",
+    component: Banner,
+};
+
+export const Primary = () => <Banner />;
